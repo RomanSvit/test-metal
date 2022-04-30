210b3153494995ec9f117fe8a3d728c7d39ce345
Roman <roman.svitelskyi@gmail.com>
2022-04-30 13:01:54 +0300

    all changes

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,6 +1,7 @@
 import React from "react";
-import "./App.scss";
 import Header from "../header/Header";
+import "./App.scss";
+
 
 const App = () => {
   return (
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,27 +1,12 @@
-import React from "react";
-import LangSelect from "./langSelect/LangSelect";
+import React from 'react';
 import "./Header.scss";
-import NavDesck from "./navDesck/NavDesck";
-import Slider from "./slider/Slider";
-const Header = () => {
-  return (
-    <>
-      <header className="header">
-        <div className="container">
-          <div className="wraper-header-nav">
-            <span className="logo"></span>
-            <NavDesck />
-            <div className="wrapp">
-              <button className="search-btn"></button>
-              <LangSelect />
-            </div>
-          </div>
-        </div>
-        
-        <Slider />
-      </header>
-    </>
-  );
-};
+
+function Header() {
+    return (
+        <header>
+            <p>hello world</p>
+        </header>
+    );
+}
 
 export default Header;
